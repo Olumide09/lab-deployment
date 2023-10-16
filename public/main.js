1aa10e62c9a4e418b5cdca6ccbf63be7203911bb
Olumide Olaoye <92790819+Olumide09@users.noreply.github.com>
2023-10-16 15:37:56 -0500

    deployment for cars website

---
+++ public/main.js
@@ -0,0 +1,21 @@
+let carBtn = document.querySelector('button')
+
+let myDiv = document.querySelector('div')
+
+
+carBtn.addEventListener('click', () => {
+    axios.get('/cars')
+    .then((response) => {
+        let carName = response.data
+
+        myDiv.innerHTML = ''
+
+
+        let newP = document.createElement('p')
+        newP.innerHTML = carName
+        myDiv.appendChild(newP)
+    })
+    .catch ((error) => {
+        console.log(error)
+    })
+})
